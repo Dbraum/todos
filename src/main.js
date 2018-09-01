06c77fd3c4aff844255c0f59427ec3b81eec4882
bulong <bulong.wkp@dtyunxi.com>
2018-09-01 22:45:39 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+import App from "./App";
+import router from './router'
 
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
